ac8a699d547d41093f21aebcc0a02dd15748f0be
bigyihsuan <bigyihsuan@gmail.com>
2020-07-15 11:55:23 -0400

    move things

---
+++ DVCargoMod/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using UnityModManagerNet;
+using HarmonyLib;
+
+namespace DVCargoMod
+{
+	class Main
+	{
+		private static UnityModManager.ModEntry modEntry;
+
+		static void Load(UnityModManager.ModEntry modEntry)
+		{
+			var harmony = new Harmony(modEntry.Info.Id);
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+		}
+	}
+}
